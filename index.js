6879a526dcccb1164dc8624a23d72b6ece4e188f
wungsen <fantasy.ws@gmail.com>
2016-09-29 18:52:48 +0800

    update lesson9 about menu

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var app = require('express')(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    //配置文件
+    conf = require('./lib/config'),
+    //wechat API 功能包
+    wechat = require('./lib/wechat');
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+//初始化wechat API
+wechat(conf.wechat);
+
+//创建自定义菜单，lib/menu.json 为菜单结构描述文件
+//相关文档： https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141013&token=&lang=zh_CN
+wechat.createMenu(require('./lib/menu.json'));
+
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
 app.listen(8013, function(err) {
-    console.log('listenning at 8013');
-}) //处理监听的回调
+    console.log('listening at 8013...');
+})
